dbb5bd384f1dfa3fb3f8ab21c9fa39067756aa7e
Phi Dinh <phidv9855@gmail.com>
2020-04-09 00:26:17 +0700

    Create Authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class AuthController extends Controller
+{
+    public function index() {
+        return view('home.index');
+    }
+}
